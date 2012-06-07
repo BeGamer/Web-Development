41db799696af88c3be56340dc6f5afc9a992cfe8
Kyron Taylor <begamer26@gmail.com>
2012-06-07 23:23:18 +0100

    Humble Homebrew 2.0
    Unfinished

---
+++ Humble Homebrew 2.0/index.php
@@ -0,0 +1,48 @@
+<html> 
+<head>
+<title>The Humble Homebrew Collection :: Home Page</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class='Header' align='center'>
+<a href='http://www.eff.org/'>
+	<img src='https://www.eff.org/files/EFF-logo-trans.png' width='5%' style='position: absolute; left: 10; top: 10;' class='logo' border='0'/>
+</a>
+<img src='images/logo.png' height='100%' />
+<a href='http://gitbrew.org/'>
+	<img src='http://humblehomebrew.com/Images/source-logo.png' style='position: absolute; right: 10; top :10;' width='5%' border='0' class='logo'/>
+</a>
+</div>
+<div class='container'>
+<div class='Body' align='center'>
+<p style='font-family: arial; font-weight: bold; font-size: 14; width: 95%;'> <br/>What is The Humble Hombrew Collection?</p>
+<p style='font-family: arial; font-size: 12; width: 95%;'><br/>
+The Humble Homebrew Collection is an initiative that aims to convince Sony to provide us with a legitimate and official way to create homebrew applications for the consoles that we own.
+We are providing you with a free homebrew game that aims to be polished and look professionally made and include a variety of games. We've tried to make this homebrew games collection as good as possible so that even the anti-homebrew purists will be jealous of it.
+Homebrew does not equal piracy, and this is proof of it. These games are all free for you to enjoy.
+You are free to download these games for the platform of your choice, whether it's on a jailbroken PS3, Linux, Windows, Mac or Android (where available).
+This Homebrew game collection includes Simon Tatham's Portable Puzzle Collection, Free Heroes 2 and Scogger HD ported to the Playstation 3 system. More games are being written and this collection will only increase with time.
+These are some of the first homebrew games that are released on the PS3 which are above the "proof of concept" status and is not yet another backup manager, FTP server or a port of an existing emulator.
+The main purpose of this website it to serve as a petition against Sony's unjust behavior towards their customers. You are encouraged to sign the petition, donate to the developers or you can simply download the games. The choice is yours.
+If you wish to do so, you may also donate to the developers of this homebrew application, as well as to the EFF, which helps defend our rights in this digital age.
+It is time to vote with your money, boycott Sony, request your legal homebrew and request your freedom and let them know that they have much to gain by giving us access to the homebrew that we deserve.
+Let Sony know that, as a customer, you want homebrew games, you want to see the real purpose of the Copyright Law used properly: To encourage creativity and innovation. Let them know that, as a customer, you are willing to pay for quality homebrew and that if they wanted to, they can get a share of that money. After all, all they care about is money!
+
+Thank you
+</p>
+<br/>
+<div class='Stats'>
+<p style='padding-top: 10; font-family: arial; font-weight: bold; font-size: 14; width: 95%;'>Statistics</p>
+<hr width='90%'/>
+<br/><br/>INSERT STATS CODE HERE<br/><br/><br/>
+<hr width='90%'/>
+<br/><br/>
+</div>
+</div>
+<br/><br/><br/>
+<div class='Footer' align='center'>
+The Humble Homebrew Collection: Created by Youness Alaoui - <a href='credits.html' style='color: white;'>Credits</a>
+</div>
+</div>
+</body>
+</html>
